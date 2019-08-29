370de87507986215afaff7278a60dca2b09799e6
Carlos <30105417+carlospalo@users.noreply.github.com>
2019-08-29 00:46:17 -0500

    issue with credentials fixed

--- models/index.js
+++ models/index.js
@@ -8,12 +8,16 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (process.env.use_env_variable) {
-  var sequelize = new Sequelize(process.env.use_env_variable, config);
-} else {
+// if (process.env.use_env_variable) {
+//   var sequelize = new Sequelize(process.env.use_env_variable,process.env.dialect, config);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+if(config.use_env_variable){
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+}else{
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs
   .readdirSync(__dirname)
   .filter(file => {
